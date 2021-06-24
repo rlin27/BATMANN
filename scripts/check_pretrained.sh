383eabaa4221616957e2e31d9c9cd26c1aaaf4f2
RuiLIN0212 <ruilin0212@gmail.com>
2021-06-24 13:14:00 +0800

    add new scripts

--- scripts/check_pretrained.sh
+++ scripts/check_pretrained.sh
@@ -1,31 +1,31 @@
 # check the full-precision one
-python main.py \
---log_dir ./log_check_pretrained/full_precision \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py \
+--log_dir ./log_check_pretrained/full_precision_100_5 \
 --data_dir /mnt/nfsdisk/jier/4_MANN/mann_hdv/data \
 --input_channel 1 \
 --feature_dim 512 \
---class_num 20 \
+--class_num 100 \
 --num_shot 5 \
 --pool_query_test 15 \
 --batch_size_test 4 \
 --test_episode 1000 \
 --quantization 0 \
 --test_only 1 \
---pretrained_dir ./log_full_precision/model_best.pth \
---gpu 2,3 \
+--pretrained_dir ./log_full_precision_100_5/model_best.pth \
+--gpu 0,1,2,3 \
 
-# check the binary one
-python main.py \
---log_dir ./log_check_pretrained/binary \
---data_dir /mnt/nfsdisk/jier/4_MANN/mann_hdv/data \
---input_channel 1 \
---feature_dim 512 \
---class_num 20 \
---num_shot 5 \
---pool_query_test 15 \
---batch_size_test 4 \
---test_episode 1000 \
---quantization 1 \
---test_only 1 \
---pretrained_dir ./log_binary/model_best.pth \
---gpu 2,3
+## check the binary one
+#python main.py \
+#--log_dir ./log_check_pretrained/binary_100_5 \
+#--data_dir /mnt/nfsdisk/jier/4_MANN/mann_hdv/data \
+#--input_channel 1 \
+#--feature_dim 512 \
+#--class_num 100 \
+#--num_shot 5 \
+#--pool_query_test 15 \
+#--batch_size_test 4 \
+#--test_episode 1000 \
+#--quantization 1 \
+#--test_only 1 \
+#--pretrained_dir ./log_binary_100_5/model_best.pth \
+#--gpu 2,3
